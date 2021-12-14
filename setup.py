785f1e45c4abbd1ca9f133d20e249ce6529ab16f
estefanysuarez <laura.suarez@mail.mcgill.ca>
2021-12-14 13:26:38 -0500

    initial public commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+import sys
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
+import versioneer
+
+if __name__ == "__main__":
+    setup(name='conn2res',
+          version=versioneer.get_version(),
+          cmdclass=versioneer.get_cmdclass())
