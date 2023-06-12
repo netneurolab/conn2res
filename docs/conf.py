7fe7b9145db9e936b058f0682ca4e78df9d47fbb
estefanysuarez <laura.suarez@mail.mcgill.ca>
2023-06-12 12:45:33 -0400

    change to docs conf file

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
-    'sphinxarg.ext',
+    # 'sphinxarg.ext',
     'sphinx_gallery.gen_gallery'
 ]
 
@@ -69,7 +69,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
