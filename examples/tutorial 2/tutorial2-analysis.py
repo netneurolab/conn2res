1288c709142d8f9987609bbd00b1f69ea3d4eaca
estefanysuarez <laura.suarez@mail.mcgill.ca>
2023-08-23 10:26:46 -0400

    [ENH] updated tutorial 2

--- examples/tutorial 2/tutorial2-analysis.py
+++ examples/tutorial 2/tutorial2-analysis.py
@@ -11,61 +11,65 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-from conn2res import plotting
-
 PROJ_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-OUTPUT_DIR = os.path.join(PROJ_DIR, 'results', 'results_new_generated_iodata', 'results_rsn_gain_15x0.0001')
-# OUTPUT_DIR = os.path.join(PROJ_DIR, 'results', 'results_old_iodata_sig-and-relia', 'results_rsn_gain_15')
+OUTPUT_DIR = '/Users/laurasuarez/Library/CloudStorage/OneDrive-McGillUniversity/new_results/T2-1000trials_0.0001gain/human'  #os.path.join(PROJ_DIR, 'results', 'T2-1000trials_0.0001gain', 'human')
 
 rsn_labels = ['VIS', 'SM', 'DA', 'VA', 'LIM', 'FP', 'DMN']
 
 
-def concat_results():
+def concat_results(exp):
 
     scores = []
-    for sample_id in range(1000):
-        df = pd.read_csv(os.path.join(OUTPUT_DIR, f'res_null_{sample_id}.csv')).reset_index(drop=True)
+    for sample_id in range(500):
+        print(sample_id)
+        df = pd.read_csv(os.path.join(OUTPUT_DIR, f'{exp}_null_{sample_id}_scores.csv')).reset_index(drop=True)
         df['sample_id'] = sample_id
 
         try:
             scores.append(df[['sample_id', 'alpha', 'module', 'n_nodes', 'corrcoef']])
-        except:
+        except KeyError:
             scores.append(df[['sample_id', 'alpha', 'corrcoef']])
 
     scores = pd.concat(scores).reset_index(drop=True)
 
     scores.to_csv(
-        os.path.join(OUTPUT_DIR, 'res_null.csv'),
+        os.path.join(OUTPUT_DIR, f'{exp}_null_scores.csv'),
         index=False
         )
 
-concat_results()
 
-df_emp = pd.read_csv(os.path.join(OUTPUT_DIR, 'res_empirical.csv')).reset_index(drop=True)
-df_emp_avg = df_emp[['alpha', 'corrcoef']]
-df_emp_avg = df_emp_avg.groupby('alpha').mean().reset_index()
 
-df_null  = pd.read_csv(os.path.join(OUTPUT_DIR, 'res_null.csv')).reset_index(drop=True)
-df_null_avg = df_null[['sample_id', 'alpha', 'corrcoef']]
-df_null_avg = df_null_avg.groupby(['sample_id', 'alpha']).mean().reset_index()
+# ---------- HUMAN ----------
+for exp in range(6):
+    print(f'\n--------{exp}----------')
+    concat_results(exp)
 
-sns.set(style="ticks", font_scale=1.0)
-fig = plt.figure(figsize=(15,5))
-ax = plt.subplot(111)
+for exp in range(6):
+    df_emp = pd.read_csv(os.path.join(OUTPUT_DIR, f'{exp}_empirical_scores.csv')).reset_index(drop=True)
+    df_emp_avg = df_emp[['alpha', 'corrcoef']]
+    df_emp_avg = df_emp_avg.groupby('alpha').mean().reset_index()
 
-sns.boxplot(
-    data=df_null_avg,
-    x='alpha',
-    y='corrcoef',
-    sym='',
-    ax=ax
-)
+    df_null  = pd.read_csv(os.path.join(OUTPUT_DIR, f'{exp}_null_scores.csv')).reset_index(drop=True)
+    df_null_avg = df_null[['sample_id', 'alpha', 'corrcoef']]
+    df_null_avg = df_null_avg.groupby(['sample_id', 'alpha']).mean().reset_index()
 
-sns.scatterplot(
-    x=np.arange(len(np.unique(df_null_avg['alpha']))),
-    y=df_emp_avg['corrcoef'].values,
-    ax=ax
-)
+    sns.set(style="ticks", font_scale=1.0)
+    fig = plt.figure(figsize=(15,5))
+    ax = plt.subplot(111)
 
-plt.show()
-plt.close()
+    sns.boxplot(
+        data=df_null_avg,
+        x='alpha',
+        y='corrcoef',
+        sym='',
+        ax=ax
+    )
+
+    sns.scatterplot(
+        x=np.arange(len(np.unique(df_null_avg['alpha']))),
+        y=df_emp_avg['corrcoef'].values,
+        ax=ax
+    )
+
+    plt.show()
+    plt.close()
